0d7a0b2ade7e7cb8ae4afd9c6180491f0f3e18f3
tibfox <norman.fischer@gmail.com>
2021-07-17 00:14:14 +0200

    set node descovery timeout harcoded to 1 second to avoid app freeze on startup fixes #40

--- lib/utils/discoverAPINode.dart
+++ lib/utils/discoverAPINode.dart
@@ -10,7 +10,12 @@ Future<String> discoverAPINode() async {
 
   for (var node in _nodes) {
     var _beforeRequestMicroSeconds = DateTime.now().microsecondsSinceEpoch;
-    var response = await http.get(Uri.parse(node + '/count'));
+    var response = await http
+        .get(Uri.parse(node + '/count'))
+        .timeout(Duration(seconds: 1), onTimeout: () {
+      // Time has run out, do what you wanted to do.
+      return http.Response('Error', 500); // Replace 500 with your http code.
+    });
     if (response.statusCode == 200) {
       var _afterRequestMicroSeconds = DateTime.now().microsecondsSinceEpoch;
 
