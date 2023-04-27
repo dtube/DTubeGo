cebc64a2cbbbd33520028d566f636ac1e702fd85
MrFasolo97 <79fasolo97@gmail.com>
2023-04-27 13:58:15 +0200

    Fixed a typo, added some more text.

--- lib/ui/pages/user/Widgets/MenuButton.dart
+++ lib/ui/pages/user/Widgets/MenuButton.dart
@@ -54,7 +54,7 @@ Widget buildUserMenuSpeedDial(
                   builder: (BuildContext context) => AlertDialog(
                     backgroundColor: globalBGColor,
                     title: Text("Temporarily disabled"),
-                    content: Text("Account history functionality is temporarily diabled, please use avalonblocks.com explorer instead."),
+                    content: Text("Account history functionality is temporarily disabled, please, in the mean time, use avalonblocks.com explorer instead."),
                       actions: <Widget>[
                       TextButton(
                         onPressed: () => Navigator.pop(context, 'Cancel'),
